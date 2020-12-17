d85fad8bf660a4956ae48ec457645b383b74385b
sarahrosecooper <sarahrosecooperx@gmail.com>
2020-12-17 01:28:16 -0500

    basic skeleton of InstructorRegister.js

--- my-app/src/Components/Instructor/InstructorRegister.js
+++ my-app/src/Components/Instructor/InstructorRegister.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react'
 
 
 const InstructorRegister = () => {
+
+    // ----------- INSTRUCTOR STATE -------------
+
     const [instructor, setInstructor] = useState({
+        email: "",
         username: "",
         password: "",
-        email: "",
         role: {
             role1: false,
             role2: false
@@ -13,8 +16,63 @@ const InstructorRegister = () => {
     })
 
     return (
-
+        <div>
         <div>This is the Registration Form</div>
+
+        {/* ------------- INSTRUCTOR FORM -------------------- */}
+        <form onSubmit="">
+
+        {/* -------------- E-MAIL -------------- */}
+
+             <label htmlFor="email">
+                <input 
+                id="email"
+                type="email"
+                name="email"
+                placeholder="E-mail here please."
+                />
+                </label>
+            
+            
+            {/* --------------USERNAME -------------- */}
+            
+            <label htmlFor="username">username:
+            <input
+            id="username"
+            type="text"
+            name="username"
+            placeholder="Please enter username here."
+            />
+            </label>
+
+             {/* --------------USERNAME -------------- */}
+
+             <label htmlFor="password">password:
+                <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Password here, please."
+                />
+             </label>
+
+              {/* -------------- ROLE -------------- */}
+              <label htmlFor="role">
+                Please select your role.
+                <select id="role" name="role">
+                    <option>---Please choose an option--</option>
+                    <option>Role 1</option>
+                    <option>Role 2</option>
+                </select>
+
+              </label>
+
+               {/* -------------- SUBMIT BUTTON -------------- */}
+
+               <button type="submit">Ready to register?</button>
+
+        </form>
+        </div>
     )
 }
 
